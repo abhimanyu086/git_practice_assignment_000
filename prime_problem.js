06dcb3e23cfcc3f9c6355f390483d365ec4f4d60
Guyvinay <mrsinghvinay563@gmail.com>
2022-12-07 00:35:53 +0530

    your_code_rectified

--- prime_problem.js
+++ prime_problem.js
@@ -2,14 +2,14 @@ let num=13;
 
 let count=0;
 
-for(let i=0;i<=num;i++){
+for(let i=0;i<num;i++){
   if(num%i===0){
     count++;
 
 }
 }
-    if(count===1){
+    if(count===2){
      console.log("It ia a prime");
   }else{
-    console.log(It is not a prime);
+    console.log("It is not a prime");
    }
