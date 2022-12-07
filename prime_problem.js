95397eb8080b3f49f26b4ff531467b62d47c4db4
unknown <pandeypoornima7509@gmail.com>
2022-12-07 13:08:45 +0530

    prime code fixed

--- prime_problem.js
+++ prime_problem.js
@@ -2,13 +2,13 @@ let num=13;
 
 let count=0;
 
-for(let i=0;i<num;i++){
-  if(num%i===0){
+for(let i=1;i<=num;i++){
+  if(num%i==0){
     count++;
 
 }
 }
-    if(count===2){
+    if(count==2){
      console.log("It ia a prime");
   }else{
     console.log("It is not a prime");
